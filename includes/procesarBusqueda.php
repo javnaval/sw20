4617d7bb50a462df56b701359cbb825149086cfe
DaniLedesma1 <61503116+DaniLedesma1@users.noreply.github.com>
2020-04-17 17:29:42 +0200

    Add files via upload

--- includes/procesarBusqueda.php
+++ includes/procesarBusqueda.php
@@ -14,27 +14,31 @@ $albumes = databaseFactory::getTable("Albums")->where("title", "LIKE", "%".$busc
 
 if ($canciones == null && $artistas == null && $albumes == null) echo "<p>No existen resultados para su búsqueda.</p>";
 else {
-    if ($canciones != null) echo "<h1>CANCIONES</h1>";
-    foreach ($canciones as $cancion) {
-        echo '<div>';
-        echo '<h3>' . $cancion->getTitle() . '</h3><p>Artista: ' . $cancion->getIdArtist() . '</p><p>Album: ' . $cancion->getIdAlbum() . '</p>';
-        echo '<audio src="server/' . $cancion->getId()  . '.mp3" type="audio/mpeg" controls>Tu navegador no soporta el audio</audio>';
-        echo '</div>';
-    }
+    if ($canciones != null){ 
+		echo "<h1>CANCIONES</h1>";
+		foreach ($canciones as $cancion) {
+			echo '<div>';
+			echo '<h3>' . $cancion->getTitle() . '</h3><p>Artista: ' . $cancion->getIdArtist() . '</p><p>Album: ' . $cancion->getIdAlbum() . '</p>';
+			echo '<audio src="server/' . $cancion->getId()  . '.mp3" type="audio/mpeg" controls>Tu navegador no soporta el audio</audio>';
+			echo '</div>';
+		}
+	}
 
     if ($artistas != null) {
-    echo "<h1>ARTISTAS</h1>";
-    foreach ($artistas as $artista) {
-        echo '<div>';
-        echo '<h3>' . $artista->getName() . '</h3><p>Género: ' . $artista->getGenre() . '</p>';
-        echo '</div>';
+		 echo "<h1>ARTISTAS</h1>";
+		 foreach ($artistas as $artista) {
+			  echo '<div>';
+			  echo '<h3>' . $artista->getName() . '</h3><p>Género: ' . $artista->getGenre() . '</p>';
+			  echo '</div>';
+		 }
     }
-   }
 
-    if ($albumes != null) echo "<h1>ALBUMES</h1>";
-    foreach ($albumes as $album) {
-        echo '<div>';
-        echo '<h3>' . $album->getTitle() . '</h3><p>Año de lanzamiento: ' . $album->getReleaseDate() . '</p>';
-        echo '</div>';
-    }
+    if ($albumes != null){
+		 echo "<h1>ALBUMES</h1>";
+		 foreach ($albumes as $album) {
+		     echo '<div>';
+		     echo '<h3>' . $album->getTitle() . '</h3><p>Año de lanzamiento: ' . $album->getReleaseDate() . '</p>';
+	         echo '</div>';
+		 }
+	}
 }
