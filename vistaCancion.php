7c2d3d5b492e083c749e4163d26a718fd8095606
lucialat <61499926+lucialat@users.noreply.github.com>
2020-05-01 19:35:40 +0200

    images

--- vistaCancion.php
+++ vistaCancion.php
@@ -17,7 +17,7 @@ function muestraCancion($idCancion){
     $html = "";
     $song = song::buscaSongId($idCancion);
     if ($song !== null) {
-		$html .= "<div class=\"imagen\"><img src='server/images/Colores.jpg'></div><div class=\"titulo\">";
+		$html .= ">";
         $html .= $song->getTitle();
         $html .= '</div><div class="audio"><audio src="server/songs/' . $song->getId() . '.mp3" type="audio/mpeg" controls>Tu navegador no soporta el audio</audio></div>';
 
